0833b905241e4434f7ddded1f9c8e384bf9e523d
Chaim Ashkenazi <72147395+Chaim95@users.noreply.github.com>
2023-05-15 16:21:34 +0300

    Add files via upload

---
+++ chaim and shai/generate_sponsors_scrypt.py
@@ -0,0 +1,29 @@
+import csv
+import random
+import string
+
+from faker import Faker
+import faker_commerce
+
+fake = Faker()
+fake.add_provider(faker_commerce.Provider)
+# Define the column names for the CSV file
+fieldnames = ['Sponsor_Name', 'Product', 'Trademark', 'Office_Address', 'RP_Contact', 'SponsorID']
+
+# Generate a list of dictionaries representing the sponsors
+sponsors = []
+for i in range(1, 19001):
+    sponsor = {'Sponsor_Name': fake.company(),
+               'Product': fake.ecommerce_name(),
+               'Trademark': ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)),
+               'Office_Address': fake.address(),
+               'RP_Contact': ''.join(random.choices(string.digits, k=10)),
+               'SponsorID': i}
+    sponsors.append(sponsor)
+
+# Write the list of sponsors to a CSV file
+with open('sponsors.csv', mode='w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for sponsor in sponsors:
+        writer.writerow(sponsor)
