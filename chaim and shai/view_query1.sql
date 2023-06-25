62f36f8c745c018968e5252c312716b62f0fa92c
Chaim Ashkenazi <72147395+Chaim95@users.noreply.github.com>
2023-06-26 00:42:12 +0300

    Delete view_query1.sql

--- chaim and shai/view_query1.sql
+++
@@ -1,4 +0,0 @@
-select country, sponsor_name, sponsorid, team_name, teamid, year_start, year_end
-from teamsponsorshipview
-where country = 'Israel' and (year_end >=2000 or year_end = null)
-order by(sponsor_name);
