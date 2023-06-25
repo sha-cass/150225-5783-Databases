b1ded0ad3aece75888970126bed5df24f91d3a36
Chaim Ashkenazi <72147395+Chaim95@users.noreply.github.com>
2023-06-25 20:06:07 +0300

    Update complexQuery4.sql

--- chaim and shai/1st Queries/complexQuery4.sql
+++ chaim and shai/1st Queries/complexQuery4.sql
@@ -1,5 +1,5 @@
 --teqams with sponsors from IL
-SELECT s.city, t.Team_name
+SELECT t.Team_name, s.city, s.country, sr.sponsor_name, sr.pr_contact
 FROM chashken.Stadium s
 INNER JOIN chashken.Team t ON s.StadiumID = t.StadiumID
 INNER JOIN chashken.sponsored_by sp ON t.TeamID = sp.TeamID
